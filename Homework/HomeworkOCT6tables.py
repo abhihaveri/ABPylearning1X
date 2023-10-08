a2a0e6b01f6b1a89d00d0369b03753864de19210
abhihaveri <107200338+abhihaveri@users.noreply.github.com>
2023-10-09 01:37:32 +0530

    Add files via upload

---
+++ Homework/HomeworkOCT6tables.py
@@ -0,0 +1,19 @@
+#Task 2:-
+'''Take a input from a user and print the table
+How to use the print with formatting f
+print(f''), How can print the same?'''
+number=input('Please Enter the number-tables you want to Display\n')
+number=int(number)
+print(f'The table for {number} is as Shown below')
+print(f'{number}x1={number*1}')
+print(f'{number}x2={number*2}')
+print(f'{number}x3={number*3}')
+print(f'{number}x4={number*4}')
+print(f"{number}x5={number*5}")
+print(f'{number}x6={number*6}')
+print(f'{number}x7={number*7}')
+print(f'{number}x8={number*8}')
+print(f'{number}x9={number*9}')
+print(f'{number}x10={number*10}')
+
+

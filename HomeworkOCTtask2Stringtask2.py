4771a29a031010f279360ede88559c223d869b1e
abhihaveri <abhishekghaveri@gmail.com>
2023-10-10 02:02:02 +0530

    Initial and first commit

---
+++ HomeworkOCTtask2Stringtask2.py
@@ -0,0 +1,27 @@
+#Task 3 :
+#List the all the functions avaiable for the String Data Type.
+#e.g len()
+
+name='Abhishek is learning python'
+print('The length of name using len string function',len(name))
+print('The type of name using type string function',type(name))
+print('To check the String is alphanumeric ::',name.isalnum())
+print('To check the String is alpha ::',name.isalpha())
+print('To check the String is numeric ::',name.isnumeric())
+print('To check the String is digit ::',name.isdigit())
+print('To check the String is decimal ::',name.isdecimal())
+print('To check the String is Printable ::',name.isprintable())
+print('To check the String is having space ::',name.isspace())
+print('To check the String is Title ::',name.istitle())
+print('To check the String is upper ::',name.isupper())
+print('To check the String is lower ::',name.islower())
+print('Converts to a lower ::',name.lower())
+print('Converts to a upper ::',name.upper())
+print('Converts to a title ::',name.title())
+print('Converts to a capitalize ::',name.capitalize())
+print('Converts to a swapcase ::',name.swapcase())
+print('Replaces old sting with the New ::',name.replace('learning','begining to learn'))
+print('Find a Sting in main::',name.find("python"))
+print('count of string inside a string ::',name.count("i"))
+print('string starts with check ::',name.startswith("Abhishek"))
+print('string ends with check ::',name.endswith("python"))
